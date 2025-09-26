ca90e1de97da1347c26704c786530721388c5d4d
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2025-09-26 17:48:54 +0300

    implemented CinemaHall and MovieSession

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,57 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static Injector injector = Injector.getInstance("mate.academy");
 
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
+    public static void main(String[] args) {
+        System.out.println("Testing MovieService...");
+        Movie newMovie = new Movie();
+        newMovie.setTitle("Fast and Furious");
+        newMovie.setDescription("An action-packed movie about street racing and heists.");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(newMovie);
+
+        List<Movie> allMovies = movieService.getAll();
+        System.out.println("All Movies:");
+        allMovies.forEach(movie -> System.out.println(movie.getTitle()));
+
+        System.out.println("\nTesting CinemaHallService...");
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setDescription("IMAX Theater");
+        cinemaHall.setCapacity(200);
+        CinemaHallService cinemaHallService = (CinemaHallService)
+                injector.getInstance(CinemaHallService.class);
+        cinemaHallService.add(cinemaHall);
+
+        List<CinemaHall> allCinemaHalls = cinemaHallService.getAll();
+        System.out.println("All Cinema Halls:");
+        allCinemaHalls.forEach(hall -> System.out.println(hall.getDescription()));
+
+        System.out.println("\nTesting MovieSessionService...");
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(newMovie);
+        movieSession.setCinemaHall(cinemaHall);
+        movieSession.setShowTime(LocalDateTime.of(2025, 9, 26, 20, 0));
+        MovieSessionService movieSessionService = (MovieSessionService)
+                injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession);
+
+        List<MovieSession> availableSessions = movieSessionService
+                .findAvailableSessions(newMovie.getId(),
+                        LocalDate.of(2025, 9, 26));
+        System.out.println("Available Sessions for 'Fast and Furious' on 2025-09-26:");
+        availableSessions.forEach(session -> System.out.println("Session at: "
+                + session.getShowTime()));
     }
 }
---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,71 @@
+package mate.academy.dao.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movie session " + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public MovieSession get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> getMovieSessionQuery = session.createQuery(
+                    "from MovieSession ms left join fetch ms.movie "
+                            + "where ms.id = :id", MovieSession.class
+            );
+            getMovieSessionQuery.setParameter("id", id);
+            return getMovieSessionQuery.getSingleResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a movie session by id: " + id, e);
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> query = session.createQuery(
+                    "from MovieSession ms "
+                            + "where ms.movie.id = :movieId and ms.showTime >= :startOfDay "
+                            + "and ms.showTime < :endOfDay",
+                    MovieSession.class
+            );
+            query.setParameter("movieId", movieId);
+            query.setParameter("startOfDay", date.atStartOfDay());
+            query.setParameter("endOfDay", date.plusDays(1).atStartOfDay());
+
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Error fetching available sessions for movieId: "
+                    + movieId + " on date: " + date, e);
+        }
+    }
+}
